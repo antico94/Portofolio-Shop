4f17635dd16c99b492a4ca849a8033eb6c7f1bbe
antico94 <meiuandrei@gmail.com>
2022-03-15 15:59:36 +0200

    finished carousel

--- MyShop/shop/src/components/test-gift-card/test.jsx
+++ MyShop/shop/src/components/test-gift-card/test.jsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import css from './test.module.css';
 import {Cards} from '../../containers/gift-cards/gift-cards';
+import {animated, Spring} from 'react-spring';
 
 const TestGift = () => {
 
   const [selectionIndex, setSelectionIndex] = useState(1);
   const [selection, setSelection] = useState([]);
+  const pricesList = [50, 100, 150, 200, 500];
   const transitionEffect = {
     transition: 'all .5s',
     transform: 'scale(1) rotate(360deg)',
@@ -24,11 +26,14 @@ const TestGift = () => {
   }, [selectionIndex]);
 
   const onClickCard = (event) => {
-    setSelectionIndex(parseInt(event.target.id));
+    if (parseInt(event.target.id) !== 0 && parseInt(event.target.id) !==
+        Cards.length - 1) {
+      setSelectionIndex(parseInt(event.target.id));
+    }
   };
 
   const circleClickHandler = (ev) => {
-    ev.target.classList.add(css.activeCard);
+    setSelectionIndex(parseInt(ev) + 1);
   };
 
   return (
@@ -47,19 +52,38 @@ const TestGift = () => {
           <div className={css.carousel}>
             {selection.map(function(element) {
               if (element === selection[1]) {
-                return (<div className={css.activeCard}>
-                  <p><span
-                      className={css.cardDescription}>Theme: </span>{element.name}
-                  </p>
-                  <img src={element.photo} onClick={e => onClickCard(e)}
-                       className={css.card}
-                       style={transitionEffect}
-                       id={element.id}/>
-                </div>);
+                return (
+                    <Spring
+                        from={{
+                          transform: 'scale(0.5)',
+                        }}
+                        to={{
+                          transform: 'scale(1)',
+                        }}
+                        key={element.id}
+                        config={{delay: 100, duration: 100}}>
+                      {props =>
+                          (
+                              <animated.div style={props}
+                                            className={css.activeCard}>
+                                <p><span
+                                    className={css.cardDescription}>Theme: </span>{element.name}
+                                </p>
+                                <img src={element.photo}
+                                     onClick={e => onClickCard(e)}
+                                     className={css.card}
+                                     style={transitionEffect}
+                                     id={element.id}/>
+                              </animated.div>
+                          )}
+                    </Spring>);
+
               } else {
-                return <img src={element.photo} onClick={e => onClickCard(e)}
+                return <img src={element.photo}
+                            id={element.id}
+                            onClick={e => onClickCard(e)}
                             className={`${css.card} ${css.notActive}`}
-                            id={element.id}/>;
+                />;
               }
 
             })}
@@ -67,17 +91,44 @@ const TestGift = () => {
 
           <div className={css.circlesContainer}>
             {[...Array(8).keys()].map(function(el) {
-              return <svg
-                  onClick={e => circleClickHandler(e)}
-                  className={css.circle}
-                  xmlns="http://www.w3.org/2000/svg" width="24"
-                  height="24"
-                  style={{fill: 'rgb(135, 135, 135)'}}>
-                <path
-                    d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2z"/>
-              </svg>;
+              if (el === selectionIndex - 1) {
+                return <svg
+                    className={`${css.circle} ${css.activeCircle}`}
+                    xmlns="http://www.w3.org/2000/svg" width="24"
+                    height="24"
+                    style={{fill: 'rgb(135, 135, 135)'}}>
+                  <path
+                      d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2z"/>
+                </svg>;
+              } else {
+                return <svg
+                    className={css.circle}
+                    xmlns="http://www.w3.org/2000/svg" width="24"
+                    height="24"
+                    style={{fill: 'rgb(135, 135, 135)'}}
+                    onClick={() => {circleClickHandler(el.toString());}}>
+                  <path
+                      d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2z"/>
+                </svg>;
+              }
 
             })}
+          </div>
+
+          <div className={css.valueContainer}>
+            <h2 className={css.valueTitle}>Gift Card Value: 100 Lei</h2>
+            <div className={css.valueButtons}>
+              {pricesList.map((price) => {
+                return (
+                    <button
+                        key={price}
+                        className={css.valueButton}
+                        onClick={(e)=>{e.target.classList.add(css.valueButtonActive)}}
+                    >{price} Lei
+                    </button>);
+              })}
+              <button className={css.valueButton}>Custom Value</button>
+            </div>
           </div>
         </div>
       </div>);
